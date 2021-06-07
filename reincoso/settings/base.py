efbb2308f07ccca235ea205272b2c639890017bd
segunisreal <segunisreal.a@lifetechnologysolutions.com>
2021-06-07 14:42:00 +0100

    update

--- reincoso/settings/base.py
+++ reincoso/settings/base.py
@@ -124,21 +124,78 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGGING = {
+LOGGING_OLD = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            # 'format': '[{asctime}] {levelname} {module} {thread:d} - {message}',
+            # 'style': '{',
+            # 'datefmt': '%d-%m-%Y %H:%M:%S'
+        },
+    },
     'handlers': {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': '/Users/segun/Desktop/WORKS/0000000000_DJANGO_WORKS/TM30/reincoso/reincoso/log.log',
+            'filename': 'logs.log',
+            # 'formatter': 'verbose',
         },
     },
+    'root': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
     'loggers': {
+        'reincoso': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
         'django': {
             'handlers': ['file'],
             'level': 'DEBUG',
             'propagate': True,
+        },
+        'django.server': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
         },
     },
 }
