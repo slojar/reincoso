b11955066e9417624f9ad333d7de91d8346a6ee1
slojar <52827399+slojar@users.noreply.github.com>
2021-09-22 16:12:19 +0100

    update

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveAPIView
+from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from modules.paystack import get_paystack_link
 from .serializers import *
